bbde5702fd339543a93fb87080a4cc732072400a
aysenurtunc8888 <aysenur.tunc@bahcesehir.edu.tr>
2023-05-06 23:58:55 +0300

    sqlite

--- django_web_app/django_web_app/settings.py
+++ django_web_app/django_web_app/settings.py
@@ -77,24 +77,14 @@ WSGI_APPLICATION = 'django_web_app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'rebaapp', 
-        'USER': 'postgres',
-        'PASSWORD': '8181',
-        'HOST': '127.0.0.1', 
-        'PORT': '5432',
-    }
-}
-
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#    }
-#  }
+        'ENGINE': 'django.db.backends.sqlite3',
+    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+   }
+ }
 
 
 
